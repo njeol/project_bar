9231db6fbea7fa357aa8f49974b83eb6107878f1
njeol <njeol@hypios.com>
2010-08-05 18:42:34 +0200

    new project bar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectBar::Application.config.secret_token = 'f2e44989843145e40eb8dd062e8acd0ff9e0774be8e6cdf6633dab06bbb474ebe1d1006ecacbbc1af32c2421ee53f86b9e5b5e3bc27fbf26332348737fc71422'
