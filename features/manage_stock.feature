8a0a23fe4d80a361ad12ff840331ee76437d08bc
njeol <njeol@hypios.com>
2010-08-06 15:08:52 +0200

    a

--- features/manage_stock.feature
+++ features/manage_stock.feature
@@ -8,14 +8,14 @@ Scenario: decrease a particular alcool
   When I press the button minus
   Then the stock of vodka decrease of one.
   
-Scenario : Send email 
+Scenario: Send a mail  
   Given I hae a stock of 50 bottles of vodka
   When I have a number of bottles bellow the number 5 
   Then A windows appear on my computer with 50 by default in the casebox
   When I click on the button order
   Then A script send a mail to the provider to order the good number of vodka 
    
-Scenario : Increase a particular alcool
+Scenario: Increase a particular alcool
   Given I have 3 bottles of vodka
   And I receive my 50 bottles of vodka 
   When I enter 50 bottles in the casebox
---
+++ features/step_definitions/manage_step.rb
@@ -0,0 +1,51 @@
+Given /^I have (\d+) by default in the casebox vodka$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I press the button minus$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the stock of vodka decrease of one\.$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I hae a stock of (\d+) bottles of vodka$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I have a number of bottles bellow the number (\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^A windows appear on my computer with (\d+) by default in the casebox$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I click on the button order$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^A script send a mail to the provider to order the good number of vodka$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have (\d+) bottles of vodka$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I receive my (\d+) bottles of vodka$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I enter (\d+) bottles in the casebox$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I press the button add to stock$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^The stock of vodka is equal to (\d+) bottles$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
