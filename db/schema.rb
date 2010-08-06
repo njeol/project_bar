585c146416a6419dc99bfaebcdd2c96f5571d79a
njeol <njeol@hypios.com>
2010-08-06 17:41:09 +0200

    formulaire alcool

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(:version => 20100806135438) do
   create_table "managers", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text      "name"
+    t.string    "name"
     t.integer   "quantity"
   end
 
